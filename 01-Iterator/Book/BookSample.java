6819c8bab1c9b32c4bbea4b075f6bae3fedac195
home <uesgknsn@gmail.com>
2019-04-07 17:22:57 +0900

    edit

--- 01-Iterator/Book/BookSample.java
+++ 01-Iterator/Book/BookSample.java
@@ -1,9 +1,11 @@
 class BookSample{
 	public static void main(String[] args){
 		BookShelf bookShelf = new BookShelf(3);
+
 		bookShelf.add("Œá”y‚Í”L‚Å‚ ‚é");
 		bookShelf.add("‹â‰Í“S“¹‚Ì–é");
 		bookShelf.add("‚»‚µ‚Ä’N‚à‚¢‚È‚­‚È‚Á‚½");
+		
 		BookShelfIterator itr = bookShelf.iterator();
 		while(itr.hasNext()){
 			Book book = (Book)itr.next();
--- README.md
+++ README.md
@@ -4,3 +4,4 @@ refer to [TECHSCORE](https://www.techscore.com/tech/DesignPattern/index.html/)
 1. Iterator
 	- Book
 	- Student
+2. Adapter
